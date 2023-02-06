71e53fbc11a57d70a4bc040c6c1025537ba41130
Bakumenko Volodymyr <volodya.bakumenko@gmial.com>
2023-02-06 19:33:16 +0200

    Написана логіка для роботи табів на js

--- oculus/js/main.js
+++ oculus/js/main.js
@@ -1,3 +1,35 @@
-$(function(){
-
+'use strict'
+// Знаходимо всі кнопки, на які ми можемо натискати
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+// ------------------------------------------------
+// Знаходимо всі блоки, які можна показувати при натисканні на кнопки
+const tabContent = document.querySelectorAll('.tabs__content-item');
+// -----------------------------------------------------------------
+// Шукаємо кнопку на яку ми клікнули
+tabItem.forEach(function(element){
+    element.addEventListener('click', open);
 });
+// ---------------------------------
+// Опис функції open
+function open(evt){
+    const tabTarget = evt.currentTarget; //Знаходимо ел. по якому клікнули
+    const button = tabTarget.dataset.button; //Знаходимо ел. якому будемо додавати клас active
+
+    // Прибираємо у всіх кнопках клас tabs__btn-item--active
+    tabItem.forEach(function(item){
+        item.classList.remove('tabs__btn-item--active');
+    });
+    // --------------------------------------------------------
+    // Прибираємо у всіх блоках клас tabs__content-item--active
+    tabContent.forEach(function(item){
+        item.classList.remove('tabs__content-item--active');
+    });
+    // --------------------------------------------------------
+    // Додаємо клас tabs__btn-item--active для тієї кнопки, на яку натиснули
+    tabTarget.classList.add('tabs__btn-item--active');
+    // ---------------------------------------------------------------------
+    // Знаходимо блок який співпадає з ID кнопки на яку ми натиснули
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+    // -------------------------------------------------------------
+}
+// -----------------
