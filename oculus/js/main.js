156dd065cad6259f567fee3b234b6fdeb93db1f2
Bakumenko Volodymyr <volodya.bakumenko@gmial.com>
2023-03-01 19:24:49 +0200

    Написані адаптив для всих секцій окрім footer

--- oculus/js/main.js
+++ oculus/js/main.js
@@ -33,3 +33,10 @@ function open(evt){
     // -------------------------------------------------------------
 }
 // -----------------
+
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function() {
+    menu.classList.toggle('menu--active');
+});
